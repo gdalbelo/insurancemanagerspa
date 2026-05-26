694437b4b5abe9d03074ce903459a4874747d866
Gabriel <santosdalbelo@gmail.com>
2026-05-26 13:45:49 -0300

    Update userServices.js
    changes http to httos for be more secure

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const baseURL = "http://insurancemanagerapi.vercel.app";
+const baseURL = "https://insurancemanagerapi.vercel.app";
 
 export function signup(data) {
   delete data.confirmPassword;
